239447927c6f76f18cb0f7ca9951ae12bf2dec29
flosell <florian.sellmayr@gmail.com>
2014-09-08 22:27:08 +0200

    publishing deploy-script as well as artifact

--- publish.sh
+++ publish.sh
@@ -4,4 +4,5 @@ REPO_DIR=/tmp/mockrepo
 
 echo "Publishing to $REPO_DIR"
 
-cp  target/todo-backend-compojure-standalone.jar $REPO_DIR/server-snapshot.tar.gz
+cp target/todo-backend-compojure-standalone.jar $REPO_DIR/server-snapshot.tar.gz
+cp deploy.sh $REPO_DIR/deploy-server.sh
