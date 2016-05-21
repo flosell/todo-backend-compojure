c9759b77890c2244c6af05cbfd681c9a024880fa
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-05-21 21:37:02 +0200

    add smoketest

---
+++ smoketest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+if [ -z "${DEPLOY_ENVIRONMENT}" ]; then
+  echo "ERROR: No DEPLOY_ENVIRONMENT defined!"
+  exit 1
+fi
+
+if [ "${DEPLOY_ENVIRONMENT}" == "ci" ]; then
+  BACKEND_PORT=8081
+elif [ "${DEPLOY_ENVIRONMENT}" == "qa" ]; then
+  BACKEND_PORT=8091
+else
+  echo "DEPLOY_ENVIRONMENT must be ci or qa"
+fi
+URL="http://localhost:${BACKEND_PORT}/todos"
+for i in {1..10}; do 
+  if curl --silent --output /dev/null --fail ${URL}; then 
+    echo -e "\033[1m$URL is up ${URL}\033[0m"
+    exit 0
+  fi
+  echo "${URL} not up, retrying..."
+  sleep 1
+done
+
+echo -e "\033[1mCould not reach server at ${URL}\033[0m"
